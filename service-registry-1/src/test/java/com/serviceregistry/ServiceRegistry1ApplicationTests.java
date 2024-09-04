c93bc7fffcc0832fce25b36ccb9e6e11f370ad09
Deeptiwani <Deeptiwani@deeptiIntern.VERINITE1.local>
2024-09-04 18:31:43 +0530

    Initial Commit.

---
+++ service-registry-1/src/test/java/com/serviceregistry/ServiceRegistry1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.serviceregistry;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistry1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
